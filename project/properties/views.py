471c2b3ef479cd7b181c3b7313287e880389e149
lavanya <lavanyakolusu275@gmail.com>
2025-04-15 13:35:17 +0530

    reconnecting to github

--- project/properties/views.py
+++ project/properties/views.py
@@ -158,15 +158,15 @@ def index(request):
 
 def signup_view(request):
     if request.method == "POST":
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        # email = request.POST['email']
+        password = request.POST.get('password')
 
         if User.objects.filter(username=username).exists():
             messages.error(request, "Username already taken!")
             return redirect('signup')
 
-        user = User.objects.create_user(username=username, email=email, password=password)
+        user = User.objects.create_user(username=username,  password=password)
         user.save()
         login(request, user)  # Auto login after sign-up
         return redirect('index')  # Redirect to main page
